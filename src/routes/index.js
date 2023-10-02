e2f732c3540212423e73384af660d9622e2ad222
Nwachee <fuzzy245.in@gmail.com>
2023-10-02 10:22:29 +0100

    Feat: Chunk upload using Multer on local disk

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-
+import videoRoute from './video.route.js'
 const router = Router();
 
 router.get('/healthcheck', (req, res) => {
   res.status(200).json({ message: 'Server ok' });
 });
 
+
+router.use('/upload', videoRoute)
 
 export default router;
